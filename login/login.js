f2d18bfd82b0bf001286c7d965bea0476903e2b9
nazcons99 <nazerke30032006@gmail.com>
2026-02-10 23:39:15 +0800

    i saved the dates in js to a separate json file

--- login/login.js
+++ login/login.js
@@ -3,15 +3,17 @@ let surname = document.querySelector('#surname');
 let number = document.querySelector('#number');
 let password = document.querySelector('#password');
 let submit = document.querySelector('#submit');
+let city = document.querySelector('#city');
 
 let users = {};//осы жерде биздин барлык ползов сакталады
 
-function User(name1, surname ,password, number) //каждый раз новый обьект создавать етеди с новым ползов и let userге салынады 
+function User(name1, surname ,password, number, city) //каждый раз новый обьект создавать етеди с новым ползов и let userге салынады 
 {
   this.name = name1; //создали обьекты
   this.surname = surname;
   this.password = password;
   this.number = number;
+  this.city = city;
 }
 
 function createid(users){
@@ -24,9 +26,10 @@ submit.addEventListener('click', () => {
   const surnameUser = surname.value;
   const passwordUser = password.value;
   const numberUser = number.value;
+  const cityUser = city.value;
 
   //барлык алган затты 1-обьектка жинау
-  const user = new User(name1User, surnameUser,  passwordUser, numberUser)
+  const user = new User(name1User, surnameUser,  passwordUser, numberUser, cityUser)
   //poluchaem nash id
   const userId = 'User' + createid(users)
 
